7e5153fa178b264531f093ac9be677881ad3a1bf
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-22 18:39:22 -0400

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+function eqArrays(arr1, arr2) {
+    let result;
+    
+    for (let j =0; j < arr2.length; j++) {
+        if (arr1[j] === arr2[j]) {
+            result = true;
+        }else {
+            result = false;
+            return result;
+        }
+    }
+    
+    return result
+}
+
+function assertArraysEqual(arr1, arr2) {
+    // if (eqArrays(arr1, arr2)) {
+    //     console.log("they are equal")
+    // } else {
+    //     console.log("they are not equal")
+    // }
+    return eqArrays(arr1, arr2) ? console.log("they are equal") : console.log("they are not equal")
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
