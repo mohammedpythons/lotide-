fa1e67981ec4a7e88d8cbcb4b6d41c75d09a6ec4
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-28 09:01:43 -0400

    using test unit

--- head.js
+++ head.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-         return `Assertion Passed: ${actual} === ${expected}`
-    } else {
-        return `Assertion Failed: ${actual} !== ${expected}`
-    }
-
-};
+const assertEqual = require('./assertEqual');
 
 function head(arr){
     let first;
@@ -23,6 +16,3 @@ console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
 
 
 
-// TEST CODE
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
