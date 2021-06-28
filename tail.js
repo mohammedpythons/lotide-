be17c14086b9b581f719c3f4ae719fa6bb75ac7d
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-28 09:02:18 -0400

    adding test unit

--- tail.js
+++ tail.js
@@ -1,19 +1,13 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-         return `Assertion Passed: ${actual} === ${expected}`
-    } else {
-        return `Assertion Failed: ${actual} !== ${expected}`
-    }
-
-};
+const assertEqual = require('./assertEqual');
 
 
 
 function tail(arr){
-     arr.shift();
-     let newarr = arr
+     
+     let [one, ...tails] = arr
+    
    
-  return newarr
+  return tails
 };
 
 let result = tail([1, 2, 3, 4])
