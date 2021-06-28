f1e2ec1c349a6812d1110885238b75ac3ee33390
mohammedpythons <mohammedshokai99@gmail.com>
2021-06-28 13:31:36 -0400

    using mocha and chai

--- tail.js
+++ tail.js
@@ -6,11 +6,9 @@ function tail(arr){
      
      let [one, ...tails] = arr
     
+    
    
   return tails
 };
 
-let result = tail([1, 2, 3, 4])
-console.log(assertEqual(result.length, 3));
-console.log(assertEqual(result[0], 2))
-console.log(assertEqual(result[1], 3))
+module.exports = tail
